c890c59d8920ae0a8b867b7c69cabe380251520f
Pedro Macias <pin.macias@gmail.com>
2021-03-27 11:35:36 +0100

    adaptar el panel de control

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
+from gestionPedidos.models import Clientes, Articulos,Pedidos
+
+
+# crear una clase para mostrar lo que queremos de cada Modelo
+class ClientesAdmin(admin.ModelAdmin):
+    list_display=('nombre','phone','direccion')
+    # busqueda
+    search_fields=('nombre','direccion')
+
+class ArticulosAdmin(admin.ModelAdmin):
+    # crear un filtro 
+    list_filter=('seccion',)
+
+
+class PedidosAdmin(admin.ModelAdmin):
+    list_display=('numero','fecha')
+    # filtro por fecha
+    list_filter=('fecha',)
+    # filtro migas de pan
+    date_hierarchy='fecha'
+
 # Register your models here.
+
+
+
+admin.site.register(Clientes, ClientesAdmin)
+
+admin.site.register(Articulos,ArticulosAdmin)
+
+admin.site.register(Pedidos, PedidosAdmin)
--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -7,14 +7,21 @@ from django.db import models
 class Clientes (models.Model):
     nombre = models.CharField(max_length=30)
     direccion = models.CharField(max_length=50)
-    # solo direcciones de email validas
-    email= models.EmailField()
-    phone = models.CharField(max_length=9)
+    # solo direcciones de email validas y que no sea requerido
+    email= models.EmailField(blank=True,null=True)
+    phone = models.CharField(max_length=9, verbose_name='Telefono')
+
+    def __str__(self):
+        return self.nombre
 
 class Articulos(models.Model):
     nombre = models.CharField(max_length=50)
     seccion = models.CharField(max_length=50)
     precio = models.IntegerField()
+
+    # crear la funcion string 
+    def __str__(self):
+        return 'El nombre es %s la seccion es %s y el precio %s'%(self.nombre,self.seccion,self.precio)
 
 class Pedidos(models.Model):
     numero = models.IntegerField()
